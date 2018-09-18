90e0fbae9b133d75f9d78ab61728a5e630b3a8f4
unknown <andres@digitalhouse.com>
2018-09-17 21:40:48 -0300

    exercicios da aula 01

---
+++ index.js
@@ -0,0 +1,16 @@
+let positivo = 5;
+let negativo = -1;
+let decimal = 0.5;
+let texto = "texto";
+let booleano = true;
+alert(decimal);
+
+let nome = "Renato";
+let sobrenome = "Minami";
+console.log(nome,sobrenome);
+
+let texoTabulado = "o texto texoTabulado  fdedeej dede";
+console.log(texoTabulado);
+
+let digitalHouse = ["Renato","Fabio","Bernardo","André","Josef"];
+console.log(digitalHouse);
