2820d0d3cbe35470b642000527fac15a706f479a
unknown <andres@digitalhouse.com>
2018-09-18 21:17:36 -0300

    Trabalhando com arrays em js

--- aula-2/index.js
+++ aula-2/index.js
@@ -22,7 +22,30 @@ function meuSanduiche(ingrediente1, ingrediente2){
    }
 
    return frase + "\n" + callback();
-
  }
 
- alert(meuSanduiche("pão","linguiça"));
+function geraArray(){
+  let arr = [];
+  for(let i = 1; i < 21; i++ ){
+    arr.push(i);
+  }
+  return arr;
+}
+
+function multiplos(){
+  let array = geraArray;
+  array.forEach(function m7(valor){
+    if(valor % 7 == 0){
+      console.log(valor);
+    }
+  });
+}
+
+function map(){
+  let array = geraArray();
+  array.forEach(function raizQ(valor){
+    console.log(Math.sqrt(valor));
+  })
+}
+
+console.log(map());
